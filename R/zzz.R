b20f33510f8387ce1b3eb0026bcb184a4c52e538
McMahon <Mike.McMahon@dfo-mpo.gc.ca>
2024-10-28 12:33:04 -0300

    remove reference to new package

--- R/zzz.R
+++ R/zzz.R
@@ -1,10 +1,10 @@
-.onAttach <- function(libname, pkgname) {
-#   localVer = utils::packageDescription('Mar.data')$Version
-#   packageStartupMessage(paste0("Version: ", localVer))
-  packageStartupMessage("You may also want to install 'PopulationeEcologyDivision/Mar.data.extended' for a bathymetry layer (too big to add here)")
-}
-
-.onLoad <- function(libname, pkgname){
-  options(stringsAsFactors = FALSE)
-#  Mar.utils::updateCheck(gitPkg = 'Maritimes/Mar.data')
-}
+# .onAttach <- function(libname, pkgname) {
+# #   localVer = utils::packageDescription('Mar.data')$Version
+# #   packageStartupMessage(paste0("Version: ", localVer))
+#   packageStartupMessage("You may also want to install 'PopulationeEcologyDivision/Mar.data.extended' for a bathymetry layer (too big to add here)")
+# }
+#
+# .onLoad <- function(libname, pkgname){
+#   options(stringsAsFactors = FALSE)
+# #  Mar.utils::updateCheck(gitPkg = 'Maritimes/Mar.data')
+# }
